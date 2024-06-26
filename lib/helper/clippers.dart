204119f491d9b2cf961570b5cdbee464401f5ab3
Thad Donaghue <donaghue@pndlm.com>
2024-06-26 11:54:35 -0700

    added the ovalclipperupper custom clipper class to apply a curve to the reviews list appbar

--- lib/helper/clippers.dart
+++ lib/helper/clippers.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class OvalClipperUpper extends CustomClipper<Path> {
+  Radius? radius;
+
+  OvalClipperUpper(this.radius);
+
   @override
   Path getClip(Size size) {
     Path path = Path();
     path.lineTo(0, size.height);
     path.arcToPoint(
       Offset(size.width, size.height),
-      radius: Radius.elliptical(size.width/4, 4)
+      radius: radius != null ? radius! : Radius.elliptical(size.width/4, 4)
     );
     path.lineTo(size.width, 0);
 
